ec135f7ab1282bb277945967ec2c8d0491b081e8
Riya Karnwal <119318498+Riyakarnwal2004@users.noreply.github.com>
2024-03-02 20:54:39 +0530

    Update script.js

--- spotify clone/script.js
+++ spotify clone/script.js
@@ -4,7 +4,8 @@ console.log("welcome");
 let songIndex=0;
 let audioElement= new Audio('songs/1.mp3');
 let masterplay=document.getElementById('masterplay');
-let myProgressBar=document.getElementById('myProgressBar');
+let myProgressBar=document.getElementById('myProgressBar');let gif=document.getElementById('gif');
+
 let title=document.getElementById('title');
 let songs=[
     {songname:"Salam-e-ishq", filepath:"songs/1.mp3", coverPath:"covers/1.jpg"} , {songname:"Salam-e-ishq", filepath:"songs/2.mp3", coverPath:"covers/2.jpg"} ,{songname:"Salam-e-ishq", filepath:"songs/3.mp3", coverPath:"covers/3.jpg"} ,{songname:"Salam-e-ishq", filepath:"songs/4.mp3", coverPath:"covers/4.jpg"} ,{songname:"Salam-e-ishq", filepath:"songs/5.mp3", coverPath:"covers/5.jpg"} ,{songname:"Salam-e-ishq", filepath:"song/6.mp3", coverPath:"covers/6.jpg"} ,{songname:"Salam-e-ishq", filepath:"songs/7.mp3", coverPath:"covers/7.jpg"} ,{songname:"Salam-e-ishq", filepath:"songs/8.mp3", coverPath:"covers/8.jpg"}, {songname:"Salam-e-ishq", filepath:"songs/9.mp3", coverPath:"covers/9.jpg"}, {songname:"Salam-e-ishq", filepath:"songs/10.mp3", coverPath:"covers/10.jpg"}

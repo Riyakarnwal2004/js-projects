119cbc9ac42c98c0f5c8a7b716fc7818e67364f5
Riya Karnwal <119318498+Riyakarnwal2004@users.noreply.github.com>
2024-03-05 21:48:36 +0530

    Update tellmeajoke.js

--- TellMeAJoke/tellmeajoke.js
+++ TellMeAJoke/tellmeajoke.js
@@ -4,9 +4,6 @@ Thunderwea`,` Two pickles fell out of a jar onto the floo What did one say to th
 Dill with i`,` What time is it when the clock strikes 13?
 Time to get a new cloc`,` How does a cucumber become a pickle?
 It goes through a jarring experienc`]
-
 let randomindex=Math.floor(Math.random()*arr.length)
-
-
 let a=document.querySelector("span")
 a.innerHTML=arr[randomindex]
